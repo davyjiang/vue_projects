57af0d57edbaf4b94e7a7f2247532328b16f7689
姜大巍 <davy820ramsey@gmail.com>
2018-10-30 11:12:15 +1300

    import elementUI in vue_element

--- vue_element/src/main.js
+++ vue_element/src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+
+/*---import whole element ui  start */
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+/*---import whole element ui  end   */
+
 import App from './App.vue'
+
+Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
