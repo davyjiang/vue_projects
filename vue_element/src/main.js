88b42cf5dacbb40111d3a4fae35f31a391e1180d
姜大巍 <davy820ramsey@gmail.com>
2018-10-30 11:15:02 +1300

    add english lang in element

--- vue_element/src/main.js
+++ vue_element/src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 
 /*---import whole element ui  start */
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css';
 /*---import whole element ui  end   */
 
-import App from './App.vue'
+Vue.use(ElementUI, { locale })
 
-Vue.use(ElementUI);
+import App from './App.vue'
 
 new Vue({
   el: '#app',
